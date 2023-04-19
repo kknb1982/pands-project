1ab8e1134bf37bedbc5603316f103c5f86e3b9d5
Kirstin Barnett <G00425638@atu.ie>
2023-04-19 09:01:34 +0100

    code to import iris dataset

--- Analysis.py
+++ Analysis.py
@@ -4,3 +4,8 @@
 import pandas as pd
 import matplotlib as plt
 import numpy as np
+
+# Import Fisher's Iris dataset
+iris = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", 
+                   names = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"])
+print(iris.shape)
