8cf34213c14f8060762f510c9049acbbca83a73a
Kirstin Barnett <G00425638@atu.ie>
2023-04-19 10:11:13 +0100

    details about matplotlib and seaborn

--- Analysis.py
+++ Analysis.py
@@ -6,6 +6,9 @@ import matplotlib as plt
 import numpy as np
 
 # Import Fisher's Iris dataset
+colnames = "Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"
 iris = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", 
-                   names = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"])
-print(iris.shape)
+                   names = colnames)
+
+for name in colnames:
+    print (iris[name].mean())
