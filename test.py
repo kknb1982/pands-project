ca3c20db2167711cc998a60208bb848efc4fa876
Kirstin Barnett <G00425638@atu.ie>
2023-04-21 12:10:49 +0100

    testing approaches to column names

---
+++ test.py
@@ -0,0 +1,8 @@
+sepallen = "Sepal Length"
+sepalwid = "Sepal Width"
+petallen = "Petal Length"
+petalwid = "Petal Width"
+irisclass = "Class"
+
+colnames = sepallen + ", " + sepalwid +
+print (colnames)
