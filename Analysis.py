fa664603e01bd47da6f63b69bc4e5af6e0be38a7
Kirstin Barnett <G00425638@atu.ie>
2023-04-22 15:44:14 +0100

    update import items list

--- Analysis.py
+++ Analysis.py
@@ -5,7 +5,6 @@
 # Author: Kirstin Barnett
 
 from variablemodule import *
-from fielddatamodule import *
 
 # Creates a file and prints basic statistical information about each column to the file variabledata.txt
 printfielddata()
