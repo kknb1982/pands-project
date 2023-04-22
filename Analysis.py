aad1fddf3ce2734e780c3bf11bf74f8c70e71072
Kirstin Barnett <G00425638@atu.ie>
2023-04-22 15:19:26 +0100

    cleaning functions into modules

--- Analysis.py
+++ Analysis.py
@@ -1,8 +1,11 @@
-#
+# Creates a text file with basic statistical information about the variables. 
+# Creates histograms of the variables
+# Prints scatter plots of variable pairs
+
 # Author: Kirstin Barnett
 
 from variablemodule import *
 from fielddatamodule import *
 
 # Creates a file and prints basic statistical information about each column to the file variabledata.txt
-printfielddata())
+printfielddata()
