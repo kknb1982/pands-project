2a2dd0c9b5bb48b21f7da06a867586aa1e118b42
Kirstin Barnett <G00425638@atu.ie>
2023-04-22 15:34:29 +0100

    dataframe variable name changed to dataf

--- variablemodule.py
+++ variablemodule.py
@@ -19,13 +19,13 @@ def printfielddata():
             if name != species:
                 header = (f'The column title is {name}')
                 f.write(header)
-                min = str(data[name].min())
-                max = str(data[name].max())
+                min = str(dataf[name].min())
+                max = str(dataf[name].max())
                 range = (f'\nThe minimum values is: {min} \nThe maximum value is: {max}')
                 f.writelines(range)
-                mean = str(data[name].mean())
-                median = str(data[name].median())
-                mode = str(data[name].mode())
+                mean = str(dataf[name].mean())
+                median = str(dataf[name].median())
+                mode = str(dataf[name].mode())
                 averages = (f'\nMean: {mean} \nMedian: {median} \nMode: {mode}\n\n')
                 f.writelines(averages)
             else:
