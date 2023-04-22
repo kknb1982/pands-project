cb0d3e49731629e65224461500aeeb51a973b3f9
Kirstin Barnett <G00425638@atu.ie>
2023-04-22 15:24:23 +0100

    combine functions into one file

--- variablemodule.py
+++ variablemodule.py
@@ -10,5 +10,23 @@ species = "Species"
 datafields = sepallen, sepalwid, petallen, petalwid, species
 
 # Import data
-data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", 
+dataf = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", 
                    names = datafields)
+
+def printfielddata():
+    with open('variabledata.txt', 'a') as f:
+        for name in datafields:
+            if name != species:
+                header = (f'The column title is {name}')
+                f.write(header)
+                min = str(data[name].min())
+                max = str(data[name].max())
+                range = (f'\nThe minimum values is: {min} \nThe maximum value is: {max}')
+                f.writelines(range)
+                mean = str(data[name].mean())
+                median = str(data[name].median())
+                mode = str(data[name].mode())
+                averages = (f'\nMean: {mean} \nMedian: {median} \nMode: {mode}\n\n')
+                f.writelines(averages)
+            else:
+                f.close()
